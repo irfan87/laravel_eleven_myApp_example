efb32a1bdb9902932f4d764a0fca6144be9483bf
irfan87 <nerve2009@yahoo.com>
2024-10-27 01:10:10 +0800

    setup tailwindcss

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>{{ $title ?? null }} - {{ config('app.name', 'Example') }}</title>
-	<script src="https://cdn.tailwindcss.com"></script>
+  @vite(['resources/js/app.js'])
 </head>
 
 <body>
